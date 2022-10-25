eb0af78b71f213908a5268cc8004fa1361cf522f
bayush11 <70395352+bayush11@users.noreply.github.com>
2022-10-25 16:48:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,9 @@
-from flask import Flask
-import os
+mport os
+from flask import Flask, send_from_directory
 
-app = Flask(__name__, static_folder = './build', static_url_path = '/')
-@app.route('/checkin/<projectid>/<qty>')
-def checkIn_hardware(projectid, qty):
-    return f'{qty} hardware checked in to {projectid}'.format(qty = qty, projectid = projectid)
-
-@app.route('/checkout/<projectid>/<qty>')
-def checkOut_hardware(projectid, qty):
-    return f'{qty} hardware checked out to {projectid}'.format(qty = qty, projectid = projectid)
-
-@app.route('/join/<projectid>')
-def joinProject(projectid):
-    return f'Joined {projectid}'.format(projectid = projectid)
-
-@app.route('/leave/<projectid>')
-def leaveProject(projectid):
-    return f'Left {projectid}'.format(projectid = projectid)
-
-@app.route('/')
-def index():
-    return app.send_static_file('index.html')
+app = Flask(__name__, static_url_path='', static_folder='ui/build/')
+	@@ -7,4 +8,4 @@ def index():
+    return send_from_directory('ui/build/', 'index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
