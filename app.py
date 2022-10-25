7d08f129652cbfde714512335dfde0b9adeb8aa8
bayush11 <70395352+bayush11@users.noreply.github.com>
2022-10-25 16:50:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-mport os
+import os
 from flask import Flask, send_from_directory
 
 app = Flask(__name__, static_url_path='', static_folder='ui/build/')
-	@@ -7,4 +8,4 @@ def index():
+
+@app.route('/')
+def index():
     return send_from_directory('ui/build/', 'index.html')
 
 if __name__ == '__main__':
