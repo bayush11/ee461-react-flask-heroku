3afd6d3db4b39669c8a29e0fbacdcaf0fc4c9cd3
bayush11 <70395352+bayush11@users.noreply.github.com>
2022-10-25 16:58:35 -0500

    Update App.js

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,5 +1,11 @@
+import './App.css';
 
+function App() {
+  return (
     <div>
-      AMAZING BRO!
+      This useless app is currently running on <span className="hostName">{window.location.href.split('/')[2]}</span>. AMAZING BRO!
     </div>
+  );
+}
 
+export default App;
